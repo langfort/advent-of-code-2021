5bc169f32bb65c327e1d4242dc144cd2b1af3d9c
Thomas Langford <94716932+langfort@users.noreply.github.com>
2021-12-13 09:06:01 -0500

    Added solution for day 4.

---
+++ day_4/part_2.gd
@@ -0,0 +1,90 @@
+extends Reference
+
+
+var data: Array = []
+var number_map: Dictionary = {}
+var result: int = 0
+var call_numbers: Array = []
+var winning_boards: Array = []
+
+
+func _ready() -> void:
+	pass
+
+
+func start() -> void:
+	_load_input()
+	_process_input()
+	_output_result()
+
+
+func _load_input() -> void:
+	var file: File = File.new()
+	file.open("res://day_4/input", File.READ)
+	var numbers: PoolStringArray = file.get_line().split(",")
+	for number in numbers:
+		call_numbers.push_back(number.to_int())
+	var y: int = 0
+	while file.get_position() < file.get_len():
+		file.get_line() # Discard blank line
+		for i in range(5):
+			numbers = file.get_line().split(" ", false)
+			var row: Array = []
+			var x: int = 0
+			for number_string in numbers:
+				var number: int = number_string.to_int()
+				row.push_back({
+					"number": number,
+					"called": false,
+					})
+				if not number_map.has(number):
+					number_map[number] = []
+				number_map[number].push_back(Vector2(x, y))
+				x += 1
+			data.push_back(row)
+			y += 1
+
+
+func _output_result() -> void:
+	print("Day 4 Part 2 result: %d" % result)
+
+
+func _process_input() -> void:
+	for number in call_numbers:
+		if number_map.has(number):
+			for entry in number_map[number]:
+				data[entry.y][entry.x].called = true
+				var board_number: int = floor(entry.y / 5)
+				if _check_win(entry) and not winning_boards.has(board_number):
+					winning_boards.push_back(board_number)
+					if winning_boards.size() == data.size() / 5:
+						_calculate_score(entry, number)
+						return
+
+
+func _check_win(position: Vector2) -> bool:
+	var start_y: int = floor(position.y / 5) * 5
+	var count: int = 0
+
+	# Check row
+	for x in range(5):
+		if data[position.y][x].called == true:
+			count += 1
+	if count == 5:
+		return true
+	count = 0
+
+	# Check column
+	for y in range(start_y, start_y + 5):
+		if data[y][position.x].called == true:
+			count += 1
+	return count == 5
+
+
+func _calculate_score(position: Vector2, number: int) -> void:
+	var start_y: int = floor(position.y / 5) * 5
+	for y in range(start_y, start_y + 5):
+		for x in range(5):
+			if not data[y][x].called:
+				result += data[y][x].number
+	result *= number
--- main.gd
+++ main.gd
@@ -7,6 +7,8 @@ var day_2_part_1 = load("res://day_2/part_1.gd").new()
 var day_2_part_2 = load("res://day_2/part_2.gd").new()
 var day_3_part_1 = load("res://day_3/part_1.gd").new()
 var day_3_part_2 = load("res://day_3/part_2.gd").new()
+var day_4_part_1 = load("res://day_4/part_1.gd").new()
+var day_4_part_2 = load("res://day_4/part_2.gd").new()
 
 
 func _ready() -> void:
@@ -18,3 +20,6 @@ func _ready() -> void:
 
 	day_3_part_1.start()
 	day_3_part_2.start()
+
+	day_4_part_1.start()
+	day_4_part_2.start()
